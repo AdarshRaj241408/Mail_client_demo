2c2bbd2f0b186143a480e17c837d856339f0a582
Adarsh Raj <raj.adarsh2401@gmail.com>
2024-05-25 02:15:46 +0530

    removed extra stuff

--- lib/enough_mail.dart
+++ lib/enough_mail.dart
@@ -30,50 +30,50 @@ Future<void> main() async {
   // exit(0);
 }
 
-Future<void> discoverExample() async {
-  var email = 'someone@enough.de';
-  var config = await Discover.discover(email, isLogEnabled: false);
-  if (config == null) {
-    defaultLogger.e('Unable to discover settings for $email');
-  } else {
-    defaultLogger.i('Settings for $email:');
-    if (config.emailProviders != null) {
-      for (var provider in config.emailProviders!) {
-        defaultLogger.i('provider: ${provider.displayName}');
-        defaultLogger.i('provider-domains: ${provider.domains}');
-        defaultLogger.i('documentation-url: ${provider.documentationUrl}');
-        defaultLogger.i('Incoming:');
-        defaultLogger.i(provider.preferredIncomingServer);
-        defaultLogger.i('Outgoing:');
-        defaultLogger.i(provider.preferredOutgoingServer);
-      }
-    } else {
-      defaultLogger.w('No email providers found.');
-    }
-  }
-}
+// Future<void> discoverExample() async {
+//   var email = 'someone@enough.de';
+//   var config = await Discover.discover(email, isLogEnabled: false);
+//   if (config == null) {
+//     defaultLogger.e('Unable to discover settings for $email');
+//   } else {
+//     defaultLogger.i('Settings for $email:');
+//     if (config.emailProviders != null) {
+//       for (var provider in config.emailProviders!) {
+//         defaultLogger.i('provider: ${provider.displayName}');
+//         defaultLogger.i('provider-domains: ${provider.domains}');
+//         defaultLogger.i('documentation-url: ${provider.documentationUrl}');
+//         defaultLogger.i('Incoming:');
+//         defaultLogger.i(provider.preferredIncomingServer);
+//         defaultLogger.i('Outgoing:');
+//         defaultLogger.i(provider.preferredOutgoingServer);
+//       }
+//     } else {
+//       defaultLogger.w('No email providers found.');
+//     }
+//   }
+// }
 
 /// Low level IMAP API usage example
-Future<void> imapExample() async {
-  final client = ImapClient(isLogEnabled: false);
-  try {
-    await client.connectToServer(imapServerHost, imapServerPort,
-        isSecure: isImapServerSecure);
-    await client.login(userName, password);
-    final mailboxes = await client.listMailboxes();
-    defaultLogger.i('mailboxes: $mailboxes');
-    await client.selectInbox();
-    // fetch 10 most recent messages:
-    final fetchResult = await client.fetchRecentMessages(
-        messageCount: 10, criteria: 'BODY.PEEK[]');
-    for (final message in fetchResult.messages) {
-      defaultLoggerMessage(message);
-    }
-    await client.logout();
-  } on ImapException catch (e) {
-    defaultLogger.e('IMAP failed with $e');
-  }
-}
+// Future<void> imapExample() async {
+//   final client = ImapClient(isLogEnabled: false);
+//   try {
+//     await client.connectToServer(imapServerHost, imapServerPort,
+//         isSecure: isImapServerSecure);
+//     await client.login(userName, password);
+//     final mailboxes = await client.listMailboxes();
+//     defaultLogger.i('mailboxes: $mailboxes');
+//     await client.selectInbox();
+//     // fetch 10 most recent messages:
+//     final fetchResult = await client.fetchRecentMessages(
+//         messageCount: 10, criteria: 'BODY.PEEK[]');
+//     for (final message in fetchResult.messages) {
+//       defaultLoggerMessage(message);
+//     }
+//     await client.logout();
+//   } on ImapException catch (e) {
+//     defaultLogger.e('IMAP failed with $e');
+//   }
+// }
 
 /// Low level SMTP API example
 Future<void> smtpExample(
@@ -110,49 +110,49 @@ Future<void> smtpExample(
 }
 
 /// Low level POP3 API example
-Future<void> popExample() async {
-  final client = PopClient(isLogEnabled: false);
-  try {
-    await client.connectToServer(popServerHost, popServerPort,
-        isSecure: isPopServerSecure);
-    await client.login(userName, password);
-    // alternative login:
-    // await client.loginWithApop(userName, password); // optional different login mechanism
-    final status = await client.status();
-    defaultLogger.i(
-        'status: messages count=${status.numberOfMessages}, messages size=${status.totalSizeInBytes}');
-    final messageList = await client.list(status.numberOfMessages);
-    defaultLogger.i(
-        'last message: id=${messageList.first.id} size=${messageList.first.sizeInBytes}');
-    var message = await client.retrieve(status.numberOfMessages);
-    defaultLoggerMessage(message);
-    message = await client.retrieve(status.numberOfMessages + 1);
-    defaultLogger.i('trying to retrieve newer message succeeded');
-    await client.quit();
-  } on PopException catch (e) {
-    defaultLogger.e('POP failed with $e');
-  }
-}
+// Future<void> popExample() async {
+//   final client = PopClient(isLogEnabled: false);
+//   try {
+//     await client.connectToServer(popServerHost, popServerPort,
+//         isSecure: isPopServerSecure);
+//     await client.login(userName, password);
+//     // alternative login:
+//     // await client.loginWithApop(userName, password); // optional different login mechanism
+//     final status = await client.status();
+//     defaultLogger.i(
+//         'status: messages count=${status.numberOfMessages}, messages size=${status.totalSizeInBytes}');
+//     final messageList = await client.list(status.numberOfMessages);
+//     defaultLogger.i(
+//         'last message: id=${messageList.first.id} size=${messageList.first.sizeInBytes}');
+//     var message = await client.retrieve(status.numberOfMessages);
+//     defaultLoggerMessage(message);
+//     message = await client.retrieve(status.numberOfMessages + 1);
+//     defaultLogger.i('trying to retrieve newer message succeeded');
+//     await client.quit();
+//   } on PopException catch (e) {
+//     defaultLogger.e('POP failed with $e');
+//   }
+// }
 
-void defaultLoggerMessage(MimeMessage message) {
-  defaultLogger
-      .i('from: ${message.from} with subject "${message.decodeSubject()}"');
-  if (!message.isTextPlainMessage()) {
-    defaultLogger.i(' content-type: ${message.mediaType}');
-  } else {
-    final plainText = message.decodeTextPlainPart();
-    if (plainText != null) {
-      final lines = plainText.split('\r\n');
-      for (final line in lines) {
-        if (line.startsWith('>')) {
-          // break when quoted text starts
-          break;
-        }
-        defaultLogger.i(line);
-      }
-    }
-  }
-}
+// void defaultLoggerMessage(MimeMessage message) {
+//   defaultLogger
+//       .i('from: ${message.from} with subject "${message.decodeSubject()}"');
+//   if (!message.isTextPlainMessage()) {
+//     defaultLogger.i(' content-type: ${message.mediaType}');
+//   } else {
+//     final plainText = message.decodeTextPlainPart();
+//     if (plainText != null) {
+//       final lines = plainText.split('\r\n');
+//       for (final line in lines) {
+//         if (line.startsWith('>')) {
+//           // break when quoted text starts
+//           break;
+//         }
+//         defaultLogger.i(line);
+//       }
+//     }
+//   }
+// }
 
 class EmailService {
   static void sendEmail(String from, String to, String subject, String body) {
