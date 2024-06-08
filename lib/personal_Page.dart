1fbdf6aa629de2d6e3b315896bc4bed8784aa0cf
Adarsh Raj <raj.adarsh2401@gmail.com>
2024-06-08 20:55:51 +0530

    minute changes

--- lib/personal_Page.dart
+++ lib/personal_Page.dart
@@ -11,91 +11,132 @@ class personalPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromRGBO(143, 148, 251, 1),
-          title: Row(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(143, 148, 251, 1),
+        title: Row(
+          children: [
+            Expanded(
+              child: SizedBox(
+                width: 5,
+              ),
+            ),
+            SizedBox(
+              width: 25,
+            ),
+            Icon(Icons.delete_outline_outlined),
+            SizedBox(
+              width: 20,
+            ),
+            Icon(CupertinoIcons.folder_badge_plus),
+            SizedBox(
+              width: 20,
+            ),
+            Icon(CupertinoIcons.ellipsis_vertical),
+          ],
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
             children: [
-              Expanded(
-                child: SizedBox(
-                  width: 5,
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      "${email.subject}",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      CupertinoIcons.star,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text('to me'),
+                  SizedBox(
+                    width: 4,
+                  ),
+                  Icon(Icons.arrow_drop_down),
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                constraints: BoxConstraints(minHeight: 510),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "${email.body}\n\n",
+                      style: TextStyle(
+                        fontSize: 17,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(
-                width: 25,
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(143, 148, 251, 1),
+                      ),
+                      child: Text(
+                        'Reply',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(143, 148, 251, 1),
+                      ),
+                      child: Text(
+                        'Forward',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(143, 148, 251, 1),
+                      ),
+                      child: Text(
+                        'Delete',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              Icon(Icons.delete_outline_outlined),
-              SizedBox(
-                width: 20,
-              ),
-              Icon(CupertinoIcons.folder_badge_plus),
-              SizedBox(
-                width: 20,
-              ),
-              Icon(CupertinoIcons.ellipsis_vertical),
             ],
           ),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(1.0),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        "${email.subject}",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        CupertinoIcons.star,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text('to me'),
-                    SizedBox(
-                      width: 4,
-                    ),
-                    Icon(Icons.arrow_drop_down),
-                  ],
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Container(
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "${email.body}\n\n",
-                        style: TextStyle(
-                          fontSize: 17,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ));
+      ),
+    );
   }
 }
